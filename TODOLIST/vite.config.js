b0fe02761d9e83184eeaad8ed580dc7d7e31040d
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-08-05 12:53:25 -0400

    update base

--- TODOLIST/vite.config.js
+++ TODOLIST/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TODOLIST",
+  base: "/ReactToToChores/TODOLIST",
   plugins: [react()],
 });
